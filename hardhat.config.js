d6cf89de739609038f84e5b4eb3a35ebd02011c2
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2022-08-08 18:05:41 +0530

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,10 +20,13 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
+      //get this url from infura.io
       url: "https://rinkeby.infura.io/v3/f4c4155e68a64eed8cd96eb9c94aa569",
+      //put the private key of rinkeby network.
       accounts: [
         "f71b0769923b8edb6e60cf9bd291a7770444a82a3b634ec0790a82e4ecb3cc12",
       ],
     },
   },
 };
+
